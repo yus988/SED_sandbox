27e4e0775707f6f0d296a0d6038ddbe529f4a6f5
yus988 <yus3594@gmail.com>
2023-12-17 21:27:53 +0900

    保存してみたがノイズが酷い。まずは正しく録音するところから

--- DLAppRealTime/main.py
+++ DLAppRealTime/main.py
@@ -5,6 +5,11 @@ import time
 import array
 import queue
 from kss_raw import Keyword_Spotting_Service
+import pyqtgraph as pg
+import sys
+import scipy.io.wavfile
+import wave
+
 
 TEST_AUDIO_FILE_PATH = "./test/left.wav"
 SAMPLE_RATE = 22050
@@ -18,6 +23,26 @@ CHUNK_MEL = 22050  # 推定したい音の長さに合わせる。1秒=sample_ra
 pred_buffer = []
 
 
+class PlotWindow:
+    def __init__(self):
+        # プロット初期設定
+        self.win = pg.GraphicsLayoutWidget(show=True)
+        self.win.setWindowTitle("realtime plot")
+        self.plt = self.win.addPlot()  # プロットのビジュアル関係
+        self.plt.setYRange(-1, 1)  # y軸の上限、下限の設定
+        self.curve = self.plt.plot()  # プロットデータを入れる場所
+
+        # アップデート時間設定
+        self.timer = pg.QtCore.QTimer()
+        self.timer.timeout.connect(self.update)
+        self.timer.start(10)  # 10msごとにupdateを呼び出し
+
+    def update(self):
+        global pred_buffer
+        buf = np.array(pred_buffer) / 32768.0
+        self.curve.setData(buf)
+
+
 class AudioInputStream:
     def __init__(self):
         # マイクインプット設定
@@ -25,9 +50,10 @@ class AudioInputStream:
         self.audio = pyaudio.PyAudio()
         self.audio_buffer_queue = queue.Queue(maxsize=CHUNK)  # メススペクトログラムから推定する用のバッファ。アプリによって長さは異なる
         self.audio_buffer_data = []  # 推論用の配列
+        self.channel = 1
         self.stream = self.audio.open(
             format=pyaudio.paInt16,
-            channels=1,
+            channels=self.channel,
             rate=SAMPLE_RATE,
             input=True,
             frames_per_buffer=CHUNK,
@@ -40,7 +66,7 @@ class AudioInputStream:
         global pred_buffer
         # 0. 取得したデータを16進数で配列化
         wave = array.array("h", in_data)
-        # 1. 取得したデータを配列に追加
+        # # 1. 取得したデータを配列に追加
         pred_buffer.extend(wave)
         # 2. 超えている場合、最初のCHUNK分を削除
         if len(pred_buffer) > CHUNK_MEL:
@@ -76,6 +102,15 @@ class AudioInputStream:
         ret = np.frombuffer(ret, dtype="int16") / 32768.0
         return ret
 
+    def RecordWav(self):
+        wavFile = wave.open("./wav/test.wav", "wb")
+        wavFile.setnchannels(self.channel)
+        wavFile.setsampwidth(2)
+        wavFile.setframerate(SAMPLE_RATE)
+        buf = np.array(pred_buffer) / 32768.0
+        wavFile.writeframes(buf)  # Python3用
+        wavFile.close()
+
 
 if __name__ == "__main__":
     # pro_size = 10
@@ -83,20 +118,24 @@ if __name__ == "__main__":
     #     pro_bar = ('=' * i) + (' ' * (pro_size - i))
     #     print('\r[{0}] {1}%'.format(pro_bar, i / pro_size * 100.), end='')
     #     time.sleep(0.5)
+    i = 0
     kss = Keyword_Spotting_Service()
     ais = AudioInputStream()
     while ais.stream.is_active():
+        ais.RecordWav()
         # print(f"{pred_buffer}")
-        if len(pred_buffer) == CHUNK_MEL:
-            buf = np.array(pred_buffer) / 32768.0
-            predicted_keyword = kss.predict(buf, SAMPLE_RATE)
-            print(f"Predicted keyword is: {predicted_keyword}")
-        else:
-            print("queue is not full")
+        
+        # if len(pred_buffer) == CHUNK_MEL:
+        #     buf = np.array(pred_buffer) / 32768.0
+        #     predicted_keyword = kss.predict(buf, SAMPLE_RATE)
+        #     print(f"Predicted keyword is: {predicted_keyword}")
+        # else:
+        #     print("queue is not full")
 
         # val = ais.AudioInput()[0]
         # rms = np.sqrt(np.mean(val**2))
         # print("\rVal = {0}".format(rms), end="")
+
         time.sleep(1)  # 推論頻度を決定
 
     ais.stream.stop_stream()
