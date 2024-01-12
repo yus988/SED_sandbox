00fd2e92975354dff67c75393a391b625959fd6b
yus988 <yus3594@gmail.com>
2024-01-12 16:29:27 +0900

    マイコン動作までじっそう

--- DLAppRealTime/main.py
+++ DLAppRealTime/main.py
@@ -10,6 +10,10 @@ import librosa
 import configparser
 import os
 import errno
+import serial
+import heapq
+import statistics
+
 
 config_ini = configparser.ConfigParser()
 config_ini_path = "config.ini"
@@ -23,6 +27,15 @@ RECORD_DURATION = float(config_ini["DEFAULT"]["record_duration"])
 INFERENCE_INTERVAL = float(config_ini["DEFAULT"]["inference_interval"])
 CHUNK_MEL = int(SAMPLE_RATE * RECORD_DURATION)  # 推定したい音の長さに合わせる。1秒=sample_rate
 
+# serial port configuration
+COM_PORT = config_ini["Serial"]["com_port"]
+COM_SPEED = config_ini["Serial"]["com_speed"]
+SERIAL_INTERVAL = float(config_ini["Serial"]["serial_interval"])
+
+# calculate magnitute
+MAX_EXTRACT_LENGTH = int(config_ini["Calc"]["max_extract_length"])
+RANGE_TO_MAX = int(config_ini["Calc"]["range_to_max"])
+
 pred_buffer = []  # 推論用のリスト
 
 
@@ -34,9 +47,27 @@ def updateWindow(xdata, ydata):
     plt.pause(0.001)
     plt.cla()
 
+
 # 取得した音声を np.array に変換＆1~-1にマップ
 def convert_buffer(arg: list) -> np.array:
     return np.array(arg) / 32768.0
+
+
+# buffer から最大の平均を取り出す。
+def get_max_mean(arg: list) -> float:
+    max_values = heapq.nlargest(MAX_EXTRACT_LENGTH, abs(arg))
+    return statistics.mean(max_values)
+
+
+# マイコン送信用に0--255の値にマップ
+def map_to_mcu(arg: float) -> int:
+    val = round(arg * RANGE_TO_MAX)
+    return val
+
+
+def convert_send_data(category, pos, id, isStereo, L_Vol, R_Vol) -> str:
+    # 改行コードを入れないと待ち受けが発生する模様
+    return f"{category},{pos},{id},{isStereo},{L_Vol},{R_Vol}\r\n"
 
 
 class AudioInputStream:
@@ -89,7 +120,9 @@ class AudioInputStream:
 
 # 連続的に推論を実行
 def pred_loop():
+    global pred_buffer
     xdata = np.linspace(0, CHUNK_MEL, CHUNK_MEL)
+    ser = serial.Serial(COM_PORT, COM_SPEED, timeout=0)
     # グラフ描画用
     ais = AudioInputStream()
     ais.start_stream()
@@ -101,22 +134,41 @@ def pred_loop():
             if len(pred_buffer) == CHUNK_MEL:
                 start = time.perf_counter()
                 # updateWindow(xdata, pred_buffer)  # 音声波形を表示 0.055秒程度かかる
-                buf = convert_buffer(pred_buffer)  # 推論用に変換 
+                buf = convert_buffer(pred_buffer)  # 推論用に変換
                 predicted_keyword = inf.classify_audio(buf)
-                print(f"Predicted keyword is: {predicted_keyword}")
+                if predicted_keyword.get("clapping") > 0.7:
+                    i += 1
+                if i == 3:
+                    mag = get_max_mean(buf)
+                    mag = map_to_mcu(mag)
+                    data = convert_send_data(0, 0, 0, 0, mag, mag)
+                    ser.write(data.encode("utf-8"))
+                    # time.sleep(SERIAL_INTERVAL)
+                    pred_buffer = []  # 連続で判定されるのを避ける
+                    i = 0
+                    convert_send_data
 
-                # if predicted_keyword == "dog":
-                #     detect = predicted_keyword
-                # else:
-                #     detect = "____"
-                # print("\rdetect: {}".format(detect), end="")
                 end = time.perf_counter()
-                print(end - start)
+                mag = get_max_mean(buf)
+                # mag = map_to_mcu(mag)
+                print(
+                    f"Predicted keyword is: {predicted_keyword}"
+                    + "\n"
+                    + "loundness is: {:.6g}".format(mag)
+                    + "\n"
+                    + "interval is:  {}".format(end - start)
+                    + "\033[2A\r",
+                    end="",
+                )
+
+                # print(end - start)
             # time.sleep(INFERENCE_INTERVAL)  # 推論頻度を決定
         except KeyboardInterrupt:
             break
+    ser.close()
     ais.stream.stop_stream()
     ais.stream.close()
+
 
 # 1回のみ、デバッグ用
 def pred_once():
